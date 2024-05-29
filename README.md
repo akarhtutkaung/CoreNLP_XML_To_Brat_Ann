47d2c5158a693e000996938251b3159adc057ec3
Akar Htut Kaung <akarthut99@gmail.com>
2024-05-28 19:34:06 -0700

    refactored code to improve readibility and performance

--- README.md
+++ README.md
@@ -1,18 +1,25 @@
 ## Converter
 
-This tool will generate the ***Enhanced++ Dependencies*** from CoreNLP into Brat ann file.
+This script converts Core NLP XML files to Brat Ann format ***Enhanced++ Dependencies***. It takes the name of the input XML file as a command-line argument and produces two output files: annotation.conf and visual.conf.
 
 Enhanced++ Dependencies includes Part-of-Speech (POS) and the relations between them (dependencies)
 
+
 ## Usage
+To use this script, simply run it from the command line, providing the name of the input XML file as an argument:
 
 ```
 python3 converter.py [fileToConvert.xml]
 ```
+
 This will generate 3 files:
   - fileToConvert.ann
   - visual.conf
   - annotation.conf
 
 ## Warning
-The purpose of this tool is to convert only one file and NOT multiple file.
+- Note that this script requires the xml.etree.ElementTree module, which is part of the Python Standard Library. Therefore, no additional dependencies are required to run this script.
+
+- Also, the script assumes that the input XML file has the correct format, with a document element containing sentences elements, each of which contains tokens and dependencies elements. If the input file has a different structure, the script may not work correctly.
+
+- The purpose of this tool is to convert only one file and NOT multiple file.
--- converter.py
+++ converter.py
@@ -2,139 +2,170 @@ try:
     import xml.etree.cElementTree as ET
 except ImportError:
     import xml.etree.ElementTree as ET
-    
+
 import sys
 import os.path
 
-n = len(sys.argv)
-if(n < 2):
-  print("Usage: python3 converter.py [fileName.xml]")
-  quit()
+def convert_core_nlp_xml_to_brat_ann(input_file_name):
+    """
+    Converts Core NLP XML to Brat Ann format.
 
-fileName = sys.argv[1]
-if(fileName[-4:] != ".xml"):
-  print("[!] Error: Wrong File Format.")
-  print("[!] File: "+fileName)
-  quit()
-  
-if(os.path.exists(fileName) == False):
-  print("[!] Error: File Does Not Exist.")
-  print("[!] File: "+fileName)
-  quit()
-  
-index = fileName.find(".xml")
-outputFileName = fileName[0:index] + ".ann"
-  
-tree = ET.ElementTree(file=fileName)
-root = tree.getroot()
-docu = root.find('document')
-sentences = docu.find('sentences').findall('sentence')
-output  = open(outputFileName, 'w')
-rtypecount = 1
-spanlist=[]
-relationList=[]
-visualListInitial=[]
-visualListFinal=[]
-visual = {}
-for sentence in sentences:
-	sentenceID = str(sentence.get('id'))
-		 
-	# Parse POS
-	tokens = sentence.find('tokens').findall('token')
-	for t in tokens:	
-		tid = 'T'+ sentenceID + str(t.get('id'))
-		pos = str(t.find('POS').text)
-		sidx = str(t.find('CharacterOffsetBegin').text)
-		eidx = str(t.find('CharacterOffsetEnd').text)
-		word = str(t.find('word').text)
-		if ":" in pos:
-			if pos not in visualListInitial:
-				visualListInitial.append(pos)
-				pos = pos.replace(":", "_")
-				visualListFinal.append(pos)
-			else:
-				pos = pos.replace(":", "_")
-		if "." in pos:
-				if pos not in visualListInitial:
-					visualListInitial.append(pos)
-					pos = pos.replace(".", "_dot")
-					visualListFinal.append(pos)
-				else:
-					pos = pos.replace(".", "_dot")
-		if "," in pos:
-			if pos not in visualListInitial:
-				visualListInitial.append(pos)
-				pos = pos.replace(",", "_comma")
-				visualListFinal.append(pos)
-			else:
-				pos = pos.replace(",", "_comma")
-		if "$" in pos:
-			if pos not in visualListInitial:
-				visualListInitial.append(pos)
-				pos = pos.replace("$", "_dollar")
-				visualListFinal.append(pos)
-			else:
-				pos = pos.replace("$", "_dollar")
-		if pos not in spanlist:
-			spanlist.append(pos)
-		output.write(tid+'\t'+pos+' '+sidx+' '+eidx+'\t'+word+'\n')
-  
-  # Parse enhanced plus plus relation
-	targetDep = 0
-	for dependency in sentence.findall('dependencies'):
-		if dependency.get('type') == 'enhanced-plus-plus-dependencies':
-			targetDep = dependency
-			break
-	for d in targetDep:
-		rword = str(d.get('type'))
-		if(rword != "root"):
-			if ":" in rword:
-				if rword not in visualListInitial:
-					visualListInitial.append(rword)
-					rword = rword.replace(":", "_")
-					visualListFinal.append(rword)
-				else:
-					rword = rword.replace(":", "_")
-			if "." in rword:
-					if rword not in visualListInitial:
-						visualListInitial.append(rword)
-						rword = rword.replace(".", "_")
-						visualListFinal.append(rword)
-					else:
-						rword = rword.replace(".", "_")
-			if "," in rword:
-				if rword not in visualListInitial:
-					visualListInitial.append(rword)
-					rword = rword.replace(",", "_")
-					visualListFinal.append(rword)
-				else:
-					rword = rword.replace(",", "_")
-			rid = 'R'+str(rtypecount)
-			rtypecount+=1
-			if rword not in relationList:
-				relationList.append(rword)
+    Args:
+        input_file_name (str): The name of the input XML file.
 
-			govidx = d.find('governor').get('idx')
-			gov = 'T'+sentenceID+str(govidx)
-			dpdidx = d.find('dependent').get('idx')
-			dpd = 'T'+sentenceID+str(dpdidx)
-			output.write(rid+'\t'+rword+' Arg1:'+gov+' Arg2:'+dpd+'\n')
-output.close()
+    Returns:
+        None
+    """
+    # Check if the input file exists and has the correct format
+    if not input_file_name.endswith(".xml"):
+        print("[!] Error: Wrong File Format.")
+        print("[!] File: " + input_file_name)
+        sys.exit(1)
 
-conf = open('annotation.conf', 'w')
-conf.write('[entities]\n')
-for sp in spanlist:
-	conf.write(sp+'\n')
-conf.write('\n[relations]\n')
-for rl in relationList:
-	conf.write(rl+'\t'+'Arg1:<ENTITY>,'+'\t'+' Arg2:<ENTITY>\n')
-conf.close()
+    if not os.path.exists(input_file_name):
+        print("[!] Error: File Does Not Exist.")
+        print("[!] File: " + input_file_name)
+        sys.exit(1)
 
-conf = open('visual.conf', 'w')
-conf.write('[labels]\n')
-for i in range(len(visualListInitial)):
-  conf.write(visualListFinal[i]+" | "+ visualListInitial[i]+"\n")
-conf.write('[drawing]\n')
-conf.write('SPAN_DEFAULT	fgColor:black, bgColor:lightgreen, borderColor:darken\n')
-conf.write('ARC_DEFAULT	color:black, dashArray:-, arrowHead:triangle-5, labelArrow:none\n')
-conf.close()
+    # Get the output file name by replacing the extension of the input file
+    output_file_name = input_file_name.replace(".xml", ".ann")
+
+    # Parse the XML file
+    tree = ET.ElementTree(file=input_file_name)
+    root = tree.getroot()
+    doc = root.find('document')
+    sentences = doc.find('sentences').findall('sentence')
+
+    # Initialize lists to store the span types, relation types, and visual configurations
+    span_types = []
+    relation_types = []
+    visual_config_initial = []
+    visual_config_final = []
+    visual = {}
+
+    # Open the output file
+    with open(output_file_name, 'w') as output:
+        # Initialize the relation type count
+        relation_type_count = 1
+
+        # Iterate over each sentence in the XML file
+        for sentence in sentences:
+            sentence_id = str(sentence.get('id'))
+
+            # Parse POS tags
+            tokens = sentence.find('tokens').findall('token')
+            for token in tokens:
+                token_id = 'T' + sentence_id + str(token.get('id'))
+                pos = str(token.find('POS').text)
+                start_idx = str(token.find('CharacterOffsetBegin').text)
+                end_idx = str(token.find('CharacterOffsetEnd').text)
+                word = str(token.find('word').text)
+
+                # Replace special characters in the POS tag
+                pos = replace_special_characters(pos, visual_config_initial, visual_config_final)
+
+                # Add the POS tag to the span types list if it's not already there
+                if pos not in span_types:
+                    span_types.append(pos)
+
+                # Write the token information to the output file
+                output.write(token_id + '\t' + pos + ' ' + start_idx + ' ' + end_idx + '\t' + word + '\n')
+
+            # Parse enhanced plus plus relations
+            target_dep = None
+            for dependency in sentence.findall('dependencies'):
+                if dependency.get('type') == 'enhanced-plus-plus-dependencies':
+                    target_dep = dependency
+                    break
+
+            if target_dep is not None:
+                for dep in target_dep:
+                    relation_word = str(dep.get('type'))
+
+                    # Skip the root relation
+                    if relation_word == "root":
+                        continue
+
+                    # Replace special characters in the relation word
+                    relation_word = replace_special_characters(relation_word, visual_config_initial, visual_config_final)
+
+                    # Add the relation word to the relation types list if it's not already there
+                    if relation_word not in relation_types:
+                        relation_types.append(relation_word)
+
+                    # Write the relation information to the output file
+                    relation_id = 'R' + str(relation_type_count)
+                    relation_type_count += 1
+                    governor_idx = dep.find('governor').get('idx')
+                    governor = 'T' + sentence_id + str(governor_idx)
+                    dependent_idx = dep.find('dependent').get('idx')
+                    dependent = 'T' + sentence_id + str(dependent_idx)
+                    output.write(relation_id + '\t' + relation_word + ' Arg1:' + governor + ' Arg2:' + dependent + '\n')
+
+    # Write the annotation configuration file
+    with open('annotation.conf', 'w') as conf:
+        conf.write('[entities]\n')
+        for span_type in span_types:
+            conf.write(span_type + '\n')
+        conf.write('\n[relations]\n')
+        for relation_type in relation_types:
+            conf.write(relation_type + '\t' + 'Arg1:<ENTITY>,' + '\t' + ' Arg2:<ENTITY>\n')
+
+    # Write the visual configuration file
+    with open('visual.conf', 'w') as conf:
+        conf.write('[labels]\n')
+        for i in range(len(visual_config_initial)):
+            conf.write(visual_config_final[i] + " | " + visual_config_initial[i] + "\n")
+        conf.write('[drawing]\n')
+        conf.write('SPAN_DEFAULT\tfgColor:black, bgColor:lightgreen, borderColor:darken\n')
+        conf.write('ARC_DEFAULT\tcolor:black, dashArray:-, arrowHead:triangle-5, labelArrow:none\n')
+
+def replace_special_characters(s, visual_config_initial, visual_config_final):
+    """
+    Replaces special characters in a string with their corresponding visual configuration.
+
+    Args:
+        s (str): The input string.
+        visual_config_initial (list): The list of initial visual configurations.
+        visual_config_final (list): The list of final visual configurations.
+
+    Returns:
+        str: The string with special characters replaced.
+    """
+    if ":" in s:
+        if s not in visual_config_initial:
+            visual_config_initial.append(s)
+            s = s.replace(":", "_")
+            visual_config_final.append(s)
+        else:
+            s = s.replace(":", "_")
+    if "." in s:
+        if s not in visual_config_initial:
+            visual_config_initial.append(s)
+            s = s.replace(".", "_dot")
+            visual_config_final.append(s)
+        else:
+            s = s.replace(".", "_dot")
+    if "," in s:
+        if s not in visual_config_initial:
+            visual_config_initial.append(s)
+            s = s.replace(",", "_comma")
+            visual_config_final.append(s)
+        else:
+            s = s.replace(",", "_comma")
+    if "$" in s:
+        if s not in visual_config_initial:
+            visual_config_initial.append(s)
+            s = s.replace("$", "_dollar")
+            visual_config_final.append(s)
+        else:
+            s = s.replace("$", "_dollar")
+    return s
+
+if __name__ == "__main__":
+    if len(sys.argv) < 2:
+        print("Usage: python3 converter.py [fileName.xml]")
+        sys.exit(1)
+
+    input_file_name = sys.argv[1]
+    convert_core_nlp_xml_to_brat_ann(input_file_name)
